eda39fd47729c034ac3520e1f6f2fddc306880bf
Somesh <someshmnda@gmail.com>
2019-02-01 16:04:56 -0500

    Update redshift.tf
    Added vpc

--- redshift.tf
+++ redshift.tf
@@ -1,26 +1,26 @@
-#This terraform script successfully starts a new cluster.
-
-
 provider "aws" {
-        access_key      = "XXXXXXXXXXXXXXXXXXXXXXXXx"
-        secret_key      = "RhNMP+ppk30V6SkaBNMVun1DEY77F06WIF60p+OL"
+        access_key      = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        secret_key      = "XXXXXXXXXXXXXXXXXXXXXXXX"
         region          = "us-east-1"
 }
 
-#uncommenting this is throwing an error, because the cluster is already deleted
-#data "aws_redshift_cluster" "som_cluster" {
-#  cluster_identifier   = "somesh-cluster"
-#}
+variable "cluster_name" {
+        description     = "Enter the Cluster Identifier:"
+}
+
+variable "snapshot_name" {
+        description     = "Enter the Snapshot name:"
+}
 
 resource "aws_redshift_cluster" "open-claims" {
-  cluster_identifier            = "som-cluster"
-  database_name                 = "mydb"
-  master_username               = "foo"
-  master_password               = "Mustbe8characters"
+  cluster_identifier            = "${var.cluster_name}"
+  #database_name                = "mydb"
+  #master_username              = "foo"
+  #master_password              = "Mustbe8characters"
   node_type                     = "dc1.large"
-  cluster_type                  = "single-node"
-  #snapshot_identifier          = "somesh-snap"
-  #snapshot_identifier          = "${data.aws_redshift_cluster.som_cluster.id}"
+  snapshot_identifier           = "${var.snapshot_name}"
   skip_final_snapshot           = "true"
-  #FinalSnapshotIdentifier      = "testing"
+  vpc_security_group_ids        = ["sg-09c627951d00a8b7b"]
+  cluster_subnet_group_name     = "rssubnetgroup"
+  availability_zone             = "us-east-1a"
 }
